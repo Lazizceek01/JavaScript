8ed08aedd91ddcbd0aa0491b4e7fc4ec0cec7f25
Lazizceek01 <azizovlaziz33@gmail.com>
2026-05-02 11:54:36 +0000

    Kodni xato qilib o'zgartirdim

--- day1.js
+++ day1.js
@@ -1,8 +1,17 @@
-console.log("Day 1 - JavaScript");
-console.log("Day 2 - JavaScript");
+// console.log("Day 1 - JavaScript");
+// console.log("Day 2 - JavaScript");
 
-// Start learning
+// // Start learning
+
+// let a = 1;
+// let b = 2;
+// let c = "Welcome to JavaScript";
+
+// console.log(a);
+
+
+// New product 
 
 let a = 1;
 let b = 2;
-let c = "Welcome to JavaScript";
+console.log(a + b); } // <-- Mana shu yerga bitta qavs qo'shing
