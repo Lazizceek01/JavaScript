9446e4abf3451cabc6635ba6149c098492835a04
Lazizceek01 <azizovlaziz33@gmail.com>
2026-09-01 19:58:17 +0500

    Variables

---
+++ lesson_three/script.js
@@ -0,0 +1,22 @@
+// Topic: Ozgaruvchilar ( Variables )
+// 2 type of variables 
+
+//  const => ozgarmaydi
+//  can't change variable 
+const name = 'Laziz!' 
+console.log(name)
+
+//  can change variable
+// let => ozgaradi 
+let age = 22;
+console.log(age)
+
+// task change variable in age 21
+
+// age = 25
+// console.log(age)
+
+// task change variable in name "Azizov Laziz"
+// name = "Azizov Laziz"
+
+// console.log(name);
